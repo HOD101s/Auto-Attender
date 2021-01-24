1e615055a47d50886b396b28f426d8c6fffcc3d4
HOD101s <manasbass.99@gmail.com>
2021-01-25 02:33:55 +0530

    added join_meet functionality

--- attender.py
+++ attender.py
@@ -10,6 +10,16 @@ class Attender:
         self.driver = webdriver.Chrome(executable_path=os.environ['CHROME_WEB_DRIVER'],options=options)
         self.driver.get("https://meet.google.com")
 
+    def join_meet(self,meetcode,camera_off=True,mic_off=True):
+        # Click Enter Meeting Code
+        self.driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[2]/div[2]/div/c-wiz/div[1]/div/div/div[1]/div').click()
+        # Enter MeetCode
+        meetcodefield = self.driver.find_element_by_xpath('//*[@id="yDmH0d"]/div[3]/div/div[2]/span/div/div[2]/div[1]/div[1]/input')
+        meetcodefield.click()
+        meetcodefield.send_keys(meetcode)
+        # Click Continue
+        self.driver.find_element_by_xpath('//*[@id="yDmH0d"]/div[3]/div/div[2]/span/div/div[4]/div[2]/div/span').click()
+
 
     def kill(self):
         self.driver.quit()
--- run.py
+++ run.py
@@ -2,3 +2,5 @@ from attender import Attender
 import time
 
 attend = Attender()
+time.sleep(5)
+attend.join_meet('')
