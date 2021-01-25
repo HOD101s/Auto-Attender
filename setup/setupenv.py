67a18205ffd396e40bb92d1da65060b66494b92b
HOD101s <manasbass.99@gmail.com>
2021-01-25 18:02:17 +0530

    updated setupenv.py

--- setup/setupenv.py
+++ setup/setupenv.py
@@ -6,7 +6,7 @@ try:
     profile_path = os.path.join(os.getcwd(), profile_path)
     os.mkdir(profile_path)
     backslash = "\\"
-    with open('.env.ex', 'w') as f:
+    with open('.env', 'w') as f:
         f.write(f'CHROME_PROFILE={profile_path.replace(backslash, backslash+backslash)}\n')
         f.write(f'CHROME_BINARY=')
     print("Now add path to chrome.exe in .env file under and continue with setupprofile.py")
