6133afe865ab1c439e5d3af0a8c296d007811ba0
HOD101s <manasbass.99@gmail.com>
2021-01-27 20:39:02 +0530

    added attend session script

---
+++ scripts/attendsession.bat
@@ -0,0 +1,8 @@
+:: Replace 'd' below with drive where script is stored. Not required if stored in c drive.
+:: d:
+call cd path\\to\\auto-attender
+:: If using python environment uncomment and change next line
+:: call activate python_environment
+call python run.py
+:End
+PAUSE
